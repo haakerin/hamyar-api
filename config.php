6ddf7e6a1fb0098c0b9f66f1080b326c8c4dbb34
amirhossein sefidgar <sefidgaramirhossein@gmail.com>
2023-07-12 10:58:41 +0330

    signup correction

--- config.php
+++ config.php
@@ -3,17 +3,26 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
 header('Access-Control-Allow-Headers: token, Content-Type');
 header('Content-Type: application/json');
+// error_reporting(0);
 // Database configuration
-$dbHost = "aberama.iran.liara.ir:31984";
-$dbUsername = "root";
-$dbPassword = "Z8Ej2Ky0P8zH1MedAVSTSzj8";
-$dbName = "hamyar";
+$isonserver = true;
+if ($isonserver) {
+    $dbHost = "hamyar-db:3306";
+    $dbUsername = "root";
+    $dbPassword = "Z8Ej2Ky0P8zH1MedAVSTSzj8";
+    $dbName = "hamyar";
+} else {
+    $dbHost = "localhost";
+    $dbUsername = "root";
+    $dbPassword = "";
+    $dbName = "hamyar";
+}
+
 
 // Create database connection
-$conn = mysqli_connect($dbHost, $dbUsername,$dbPassword, $dbName);
+$conn = mysqli_connect($dbHost, $dbUsername, $dbPassword, $dbName);
 
 //check db connected
-if (mysqli_connect_errno()) {
+if (!$conn) {
     die("connection failed" . mysqli_connect_error());
 }
-?>
--- signup.php
+++ signup.php
@@ -4,24 +4,21 @@ require_once "./functions.php";
 
 // Retrieve POST data
 if ($_SERVER['REQUEST_METHOD'] == "POST" && $info = json_decode(file_get_contents("php://input"), true)) {
-    if (!isset($info['username'], $info['email'], $info['password'])) {
+    if (!isset($info['username'], $info['email'], $info['password'], $info['name'])) {
         respond(-1, "please send all parameters (username, email, password)");
     }
     $username = input_sec($info['username']);
+    $name = input_sec($info['name']);
     $email = input_sec($info['email']);
     $password = input_sec($info['password']);
 
     // Hash the password for security
     $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
 
-    $result = mysqli_query($conn, "select * from users");
-    while ($allUsers = mysqli_fetch_assoc($result)) {
-        if ($allUsers['email'] == $email || $allUsers['username'] == $username) {
-            respond(-2, "کاربری با این مشخصات وجود دارد");
-        }
+    if (select_stmt($conn, "SELECT * FROM `users` WHERE `username` = ?", "s", $username)) {
+        respond(-2, "کاربری با این مشخصات وجود دارد");
     }
-
-    if (stmt($conn, "INSERT INTO users (username,email,password) VALUES (?,?,?)", "sss", $username, $email, $hashedPassword))
+    if (stmt($conn, "INSERT INTO users (`username`,`name`,email,`password`) VALUES (?,?,?,?)", "ssss", $username, $name, $email, $hashedPassword))
         $respond = [
             "status" => 1,
             "message" => "Sign-up successful."
