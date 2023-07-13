d62faba71b332b78b6280a4703238d01f49d39a0
amirhossein sefidgar <sefidgaramirhossein@gmail.com>
2023-07-13 17:56:34 +0330

    add get-info

--- get_info.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once "config.php";
-require_once "functions.php";
-
-if ($_SERVER['REQUEST_METHOD'] == "POST" && $info = json_decode(file_get_contents("php://input"), true)) {
-    
-}
