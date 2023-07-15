aeb67275f64848f1f192c99c98ddd346ef7e9532
amirhossein sefidgar <sefidgaramirhossein@gmail.com>
2023-07-15 08:50:36 +0330

    islogin correct

--- is-login.php
+++ is-login.php
@@ -3,9 +3,9 @@ require_once "config.php";
 require_once "functions.php";
 
 if ($_SERVER['REQUEST_METHOD'] == "POST" && $info = json_decode(file_get_contents("php://input"), true)) {
+    if(!isset($info['token'])) respond(-1, "please send token");
     $token = $info['token'];
     $userInfo = json_decode(encrypt_decrypt('decrypt', $token, 'bozi'), true);
-    if(!isset($info['token'])) respond(-1, "please send token");
     if (!select_stmt($conn, "SELECT * FROM `users` WHERE username = ?", "s", $userInfo['username'])) respond(-2, "token wrong");
     respond(1, "user loged in");
 }
